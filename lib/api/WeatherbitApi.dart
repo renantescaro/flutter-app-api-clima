42637e7a73fca6733af1f3c4d1a412f12928b0d6
renantescaro <renantescaro@yahoo.com.br>
2021-01-18 20:01:01 -0300

    pegando informações da classe config

--- lib/api/WeatherbitApi.dart
+++ lib/api/WeatherbitApi.dart
@@ -1,3 +1,4 @@
+import 'package:clima/Config.dart';
 import 'package:clima/entities/Clima.dart';
 import 'package:clima/Api/Api.dart';
 import 'dart:convert';
@@ -6,14 +7,14 @@ import 'dart:developer' as dev;
 class WeatherbitApi {
   static Future<Clima> consultarPorCidade(String cidade) async {
     dynamic resposta =
-        await Api.getHttp(Api.urlApi + '/cidade/' + cidade + '/BR');
+        await Api.getHttp(Config.urlApi + '/cidade/' + cidade + '/BR');
     dynamic respostaJson = json.decode(resposta);
     return new Clima.fromJson(respostaJson);
   }
 
   static Future<Clima> consultarPorPosicao(
       double latitude, double longitude) async {
-    dynamic resposta = await Api.getHttp(Api.urlApi +
+    dynamic resposta = await Api.getHttp(Config.urlApi +
         '/posicao/' +
         latitude.toString() +
         '/' +
