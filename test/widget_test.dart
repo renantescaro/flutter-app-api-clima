6b2ec40096d5fb0cfd8e9ce2b982c3890273cab3
renantescaro <renantescaro@yahoo.com.br>
2021-01-18 20:03:24 -0300

    primeiros testes de widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,22 +8,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:clima/main.dart';
+import 'package:clima/views/clima.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Como você deseja consultar o clima?'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.text('Concultar por localização'));
+    //await tester.pump();
   });
 }
