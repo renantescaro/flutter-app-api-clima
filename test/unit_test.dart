6362d481f2405e8ef57e440accace577b6b9d97b
renantescaro <renantescaro@yahoo.com.br>
2021-01-18 20:03:00 -0300

    criando testes unitarios para a api

---
+++ test/unit_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:clima/api/WeatherbitApi.dart';
+import 'package:clima/entities/Clima.dart';
+
+main() {
+  group('api clima', () {
+    test('retorno cidade NÃO existente', () async {
+      Clima clima = await WeatherbitApi.consultarPorCidade('aaaaaaa');
+      expect(clima.endereco, isNull);
+    });
+
+    test('retorno por nome de cidade existente', () async {
+      Clima clima = await WeatherbitApi.consultarPorCidade('Lins');
+      expect(clima.endereco.toString(), "Lins27BR");
+    });
+
+    test('retorno por posição cidade existente', () async {
+      Clima clima =
+          await WeatherbitApi.consultarPorPosicao(-21.419583, -50.077013);
+      expect(clima.endereco.toString(), "Penápolis27BR");
+    });
+  });
+}
